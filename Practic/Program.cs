ae5688c7bcf45064311f43c464f62ac1be86872b
Linarz <Linarz@bk.ru>
2021-05-19 16:28:02 +0300

    Add project files.

---
+++ Practic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practic
+{
+   public class Circle
+    {
+        public const double PI = 3.14;
+
+        private readonly double _radius;
+        public Circle(double radius)
+        {
+            _radius=radius
+        }
+        public double CalcPerimeter
+    }
+}
